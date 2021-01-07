97368ffee7ffccc54f9ec61f0d0e3a442acacd94
JonasBM <jonas@jonasbm.com.br>
2021-01-07 18:38:27 -0300

    v0.2.2

---
+++ build/precache-manifest.2cea3a55cf67bfb25b2084fe38049660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7aebcc5f2ae12995a33675d122c157f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8a402a2583296154323",
+    "url": "/static/css/main.f4bd2df7.chunk.css"
+  },
+  {
+    "revision": "a949e502795d628a9be4",
+    "url": "/static/js/2.9674a3c0.chunk.js"
+  },
+  {
+    "revision": "350f63d86b8fb7e317faf9a9d9d23a9b",
+    "url": "/static/js/2.9674a3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a402a2583296154323",
+    "url": "/static/js/main.9b69a29c.chunk.js"
+  },
+  {
+    "revision": "2d703a6a54de17507603",
+    "url": "/static/js/runtime-main.6bc178b8.js"
+  }
+]);
